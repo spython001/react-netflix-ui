14dbe17e1abca46116f5b27706a005f04cc8703b
spython <emmanuelegbo01@gmail.com>
2023-10-16 14:10:14 +0100

    added another image

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,9 @@
+import './featured.scss'
+
+export default function Featured() {
+  return (
+    <div className='featured'>
+        <img src="/assets/netImg1.png" alt="" />
+    </div>
+  )
+}
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,36 +1,12 @@
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
 export default function Home() {
   return (
     <div className="home">
-      <Navbar/>
-      <img
-        width={"100%"}
-        src="/assets/netImg1.png" 
-        alt=""
-      />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni sapiente sit rem ipsa exercitationem? Quos ex perferendis repellendus amet, quas eveniet beatae nisi ab explicabo, culpa delectus inventore nostrum quidem.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vero dolorem, odio eum non labore, quaerat blanditiis eos quisquam unde enim ex consectetur amet. Sapiente aliquid cupiditate iure quam vel!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis quia magni itaque iure, ducimus atque suscipit obcaecati aliquam quae modi sunt, vel ratione repudiandae voluptate consectetur ad. Dicta, repellendus cupiditate!
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore ipsam molestiae libero sunt vel voluptatum repudiandae ad, accusantium doloribus beatae consequatur! Ipsa sapiente necessitatibus quos modi magnam ab dolore deleniti.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic doloremque repellendus optio ullam aliquam, nostrum, soluta ut voluptatum nemo natus illum delectus exercitationem quae debitis. Natus totam animi nesciunt non.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatum corrupti hic doloremque est cumque possimus, fugit tenetur in consectetur non nihil expedita animi facere at temporibus doloribus, saepe error.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus deserunt impedit, fuga reiciendis, doloremque possimus et atque modi iusto ab quos corrupti dolorem accusamus odio vel temporibus quaerat error excepturi!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod pariatur et officiis libero exercitationem odit, temporibus incidunt, quisquam harum repellendus perspiciatis voluptatem eius alias voluptatibus recusandae autem error omnis earum.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident magni eius, error, incidunt non aperiam repudiandae fuga voluptas neque eos accusantium voluptatibus labore rerum dignissimos atque vel amet quod assumenda.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil possimus eligendi dolores, consequatur eos animi quos deserunt perspiciatis ad tenetur maiores neque illum dolore at odit dignissimos exercitationem beatae praesentium?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis voluptates ea quae vel reprehenderit quaerat officiis quisquam! Unde voluptatem nostrum, deleniti iure iusto consequatur, sit optio voluptatibus laboriosam in architecto.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis pariatur nobis molestiae ab! Eos sapiente dolor vitae a officiis voluptates dolores eius asperiores expedita, recusandae quae maxime laboriosam debitis fugit.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem aspernatur, tempore magnam consequatur amet non distinctio eveniet blanditiis nisi sunt voluptates aperiam tenetur corrupti asperiores omnis, necessitatibus, dignissimos id! Quibusdam.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quibusdam accusantium animi iure nemo natus explicabo sequi totam autem illo, exercitationem deleniti expedita tenetur ducimus vero dolor iusto dignissimos obcaecati!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium reprehenderit quos magnam eum quasi in, ratione commodi autem atque nemo, consectetur earum? Consequatur debitis ex blanditiis reiciendis sunt expedita reprehenderit.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rerum ad quo repudiandae laudantium libero facere voluptate dolorum, amet doloribus adipisci a porro beatae perferendis dignissimos debitis fugit vero id.
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, odit voluptatibus! Earum, est optio in error excepturi suscipit inventore consequuntur nesciunt odio omnis! Est quibusdam cumque ex sint, eveniet tempore.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste dolore, culpa ad odio voluptates molestias iure, aperiam nam delectus consequuntur repellat doloremque ipsa consequatur accusantium corrupti nisi beatae ea fugiat.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt unde dolores, eos quis temporibus praesentium veritatis rerum suscipit velit. Enim similique, quas blanditiis nostrum quis impedit tempore adipisci voluptatibus quos.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem itaque, similique perferendis odit ea blanditiis minima asperiores, voluptate consectetur atque corporis officiis commodi? Officia fuga eaque reiciendis, doloribus consectetur fugit?
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis deleniti iure sed totam aut, vero, architecto saepe fuga quas tenetur in praesentium incidunt quia at illo rerum odio nam quasi.
+      <Navbar/> 
+       <Featured/>
     </div>
   )
 }
