7b0ac9aa9c68f60128b53d0505c78b917795eb8d
spython <emmanuelegbo01@gmail.com>
2023-10-16 13:45:46 +0100

    modified contents of navbar folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 
-import { Notifications, Search } from '@mui/icons-material'
+import { ArrowDropDown, Notifications, Search } from '@mui/icons-material'
+import { useState } from 'react'
 import './navbar.scss'
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () =>{
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  
   return (
-    <div className='navbar'>
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
         <div className="container">
             <div className="left">
                 <img src="/assets/netflix.png" alt="" />
@@ -15,9 +24,19 @@ export default function Navbar() {
                 <span>My List</span>
             </div>
             <div className="right">
-                <Search/>
+                <Search className='icon'/>
                 <span>KID</span>
-                <Notifications/>
+                <Notifications className='icon'/>
+                <img src="/assets/netImg1.png" alt="" />
+                
+                <div className="profile">
+                    <ArrowDropDown className='icon'/>
+                    <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                </div>
+    
             </div>
         </div>
     </div>
