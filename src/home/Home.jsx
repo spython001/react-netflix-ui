9b842556797422d327cb45431a0a330aea594f8a
spython <emmanuelegbo01@gmail.com>
2023-10-16 11:35:08 +0100

    created component folder and navbar folder in it

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,10 @@
+import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
 export default function Home() {
   return (
     <div className="home">
-      hello
+      <Navbar/>
     </div>
   )
 }
