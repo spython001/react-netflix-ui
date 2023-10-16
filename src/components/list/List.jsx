6bfd0e83297b7f42d9ab243fa76bd2976cd508e2
spython <emmanuelegbo01@gmail.com>
2023-10-16 15:26:10 +0100

    created a list item component folder with jsx and scss files

---
+++ src/components/list/List.jsx
@@ -0,0 +1,9 @@
+import './list.scss'
+
+export default function List() {
+  return (
+    <div className='list'>
+        list
+    </div>
+  )
+}
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -9,6 +9,9 @@ export default function Home() {
       <Navbar/> 
       <Featured/>
       <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   )
 }
