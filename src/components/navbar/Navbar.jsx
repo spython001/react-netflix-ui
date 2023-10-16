da932679896aa1cb1c3f2807cfe617d0273c61e4
spython <emmanuelegbo01@gmail.com>
2023-10-16 12:08:20 +0100

    0added emotion packages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import { Notifications, Search } from '@mui/icons-material'
 import './navbar.scss'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-        Navbar
+        <div className="container">
+            <div className="left">
+                <img src="/assets/netflix.png" alt="" />
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New and Popular</span>
+                <span>My List</span>
+            </div>
+            <div className="right">
+                <Search/>
+                <span>KID</span>
+                <Notifications/>
+            </div>
+        </div>
     </div>
   )
 }
