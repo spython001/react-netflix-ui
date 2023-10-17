f1327a8f4dbc29ba58a8bd5a38f242d90b363917
spython <emmanuelegbo01@gmail.com>
2023-10-17 12:57:01 +0100

    modified files

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,7 +9,16 @@ export default function Login() {
             </div>
         </div>
         <div className="container">
-            
+            <form >
+              <h1>Sign In</h1>
+              <input type="email" placeholder='Email or phone number'/>
+              <input type="password" placeholder='Password'/>
+              <button className="loginButton">Sign In</button>
+              <span>New to Netflix? <b>Sign up now</b></span>
+              <small>
+                This page is protected by Google reCAPTCHA to ensure you are not a bot. <b>Learn more</b>.
+              </small>
+            </form>
         </div>
     </div>
   )
