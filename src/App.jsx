d43925189b3f337dd66caf53c459725105f1429d
spython <emmanuelegbo01@gmail.com>
2023-10-17 12:38:20 +0100

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './app.scss'
 import Home from "./pages//home/Home"
+import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Watch from './pages/watch/Watch'
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Register/>
+        <Login/>
       </div>
         
     </>
