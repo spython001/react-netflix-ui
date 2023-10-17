79cb13089a48133d7248aad637a405e6df1bbb7e
spython <emmanuelegbo01@gmail.com>
2023-10-17 11:07:23 +0100

    modfied contents of the watch folder

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -11,7 +11,7 @@ export default function Watch() {
         <video 
           className='video' 
           autoPlay 
-          progress 
+          //progress 
           controls 
           src='/assets/netVid3.mp4'
         />
