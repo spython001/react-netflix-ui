eceda82ea16d0fe3393074a1f78094c0ae790dda
spython <emmanuelegbo01@gmail.com>
2023-10-17 10:11:43 +0100

    modified contents of list item folder

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -5,6 +5,7 @@ import './listItem.scss'
 
 export default function ListItem({index}) {
   const [isHovered, setIsHovered] = useState(false);
+  const trailer = "/assets/netVid1.mp4"
 
   return (
     <div 
@@ -14,28 +15,34 @@ export default function ListItem({index}) {
       onMouseLeave={()=>setIsHovered(false)}
     >
         <img src="/assets/netItem1.png" alt="" />
+        {isHovered && (
+          <>
+          <video src={trailer} autoPlay={true} loop/>
 
-        <div className='itemInfo'>
-          <div className="icons">
-            <PlayArrow/>
-            <Add/>
-            <ThumbUpAltOutlined/>
-            <ThumbDownOutlined/>
+          <div className='itemInfo'>
+            <div className="icons">
+              <PlayArrow className='icon'/>
+              <Add className='icon'/>
+              <ThumbUpAltOutlined className='icon'/>
+              <ThumbDownOutlined className='icon'/>
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 minutes</span>
+              <span className='limit'>+16</span>
+              <span>1999</span>
+            </div>
+
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Vitae ducimus sit ad temporibus illo non magni laudantium.
+            </div>
+
+            <div className="genre">Action</div>
           </div>
+        </>
+        )}
           
-          <div className="itemInfoTop">
-            <span>1 hour 14 minutes</span>
-            <span className='limit'>+16</span>
-            <span>1999</span>
-          </div>
-
-          <div className="desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Vitae ducimus sit ad temporibus illo non magni laudantium, dolorem perspiciatis iste.
-          </div>
-
-          <div className="genre">Action</div>
-        </div>
+          
     </div>
   )
 }
